d835fc515333b06e00d2fd1be69b8179e732c816
Anton Bytsko <anton.bytsko@yandex.ru>
2023-06-04 00:20:42 +0300

    Homework 03/06/2023

---
+++ task60.py
@@ -0,0 +1,9 @@
+"""
+Дан список строк "apple", "orange", "banana", "pineapple", "grape". Отсортировать
+его в порядке, обратном алфавитному.
+"""
+
+arr = ["apple", "orange", "banana", "pineapple", "grape"]
+arr.sort()
+arr.reverse()
+print(arr)
